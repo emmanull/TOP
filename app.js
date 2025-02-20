6c8f6caeb172eac5d782238c09778135ed69810f
emmanull <erixqui@gmail.com>
2025-02-20 13:42:19 -0600

    Primer commit - Subiendo mi carpeta

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app=express();
+
+const path = require('path');
+
+const port = 3000;
+
+
+
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Configura una ruta básica
+app.get('/', (req, res) => {
+  /*res.send('¡Hola, o!');*/
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// Levanta el servidor y escucha en el puerto especificado
+app.listen(port, () => {
+  console.log(`Ser corriendo en http://localhost:${port}`);
+});
